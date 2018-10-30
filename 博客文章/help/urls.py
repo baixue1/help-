96f69b2997a7b659c01b8a7deadbbd5d366bff22
baixue <wawabaixue@sina.com>
2018-10-30 08:54:32 +0800

    first commit

--- 博客文章/help/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.contrib import admin
-from blog import views
-
-urlpatterns = [
-    url(r'^index/$', views.blog_index),
-    url(r'^admin/', admin.site.urls)
-]
